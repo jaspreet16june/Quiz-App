4dd59339f6b0a36ed1a34762f0d1e23def37d582
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-11-10 18:52:04 +0530

    Background and Money list started

--- quizapp/src/App.jsx
+++ quizapp/src/App.jsx
@@ -5,23 +5,23 @@ let App = () => {
       <div className="main">Main</div>
       <div className="pyramid">
         <ul className="moneyList">
-          <li className="moneyListItem">
+          <li className="moneyListItem active">
             <span className="moneyListItemNumber">4</span>
             <span className="moneyListItemAmount">$ 400</span>
           </li>
-          <li className="moneyListItem">
+          <li className="moneyListItem ">
             <span className="moneyListItemNumber">4</span>
             <span className="moneyListItemAmount">$ 400</span>
           </li>
-          <li className="moneyListItem">
+          <li className="moneyListItem ">
             <span className="moneyListItemNumber">4</span>
             <span className="moneyListItemAmount">$ 400</span>
           </li>
-          <li className="moneyListItem">
+          <li className="moneyListItem ">
             <span className="moneyListItemNumber">4</span>
             <span className="moneyListItemAmount">$ 400</span>
           </li>
-          <li className="moneyListItem">
+          <li className="moneyListItem ">
             <span className="moneyListItemNumber">4</span>
             <span className="moneyListItemAmount">$ 400</span>
           </li>
